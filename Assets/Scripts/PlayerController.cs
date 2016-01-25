28dc217c3cf5fc64efa3a38fdf81eabea53ca9fe
dingyling <simen_hel@hotmail.com>
2016-01-25 14:05:27 +0100

    Fixed stuck to wall, still some problems with jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,23 +12,27 @@ public class PlayerController : MonoBehaviour {
     private Rigidbody2D rb2d;
     private GameObject[] gameObjects;
     private GameObject walkedOver = null;
+    private bool isGrounded = true;
+    private float jumpDelay;
+    private bool hasJumped;
 
     void Start()
     {
+        hasJumped = false;
         rb2d = GetComponent<Rigidbody2D>();
         Board = GameObject.FindGameObjectsWithTag("GameController");
         levelBuilder = Board[0].GetComponent<LevelBuilder>();
     }
 
-    void Update () {
-        if(Input.GetKey("left"))
+    void Update() {
+        if (Input.GetKey("left"))
         {
-            if(rb2d.velocity.x > -maxSpeed)
+            if (rb2d.velocity.x > -maxSpeed)
             {
                 rb2d.AddForce(new Vector2(-speed, 0));
             }
-            transform.eulerAngles = new Vector2(0,180);
-        } 
+            transform.eulerAngles = new Vector2(0, 180);
+        }
 
         if (Input.GetKey("right"))
         {
@@ -36,12 +40,21 @@ public class PlayerController : MonoBehaviour {
             {
                 rb2d.AddForce(new Vector2(speed, 0));
             }
-            transform.eulerAngles = new Vector2(0,0);
+            transform.eulerAngles = new Vector2(0, 0);
         }
 
-        if (Input.GetKey("up"))
+        if (Input.GetKeyDown("up"))
         {
-            Jump();
+            //if (Physics2D.Raycast(transform.position, Vector3.down, 1.0f) == true ) {
+            if (isGrounded)
+            {
+                Jump();
+                hasJumped = true;
+                jumpDelay = Time.time;
+                isGrounded = false;
+            }
+            
+            //}
         }
 
         if (Input.GetKey("left ctrl"))
@@ -51,7 +64,7 @@ public class PlayerController : MonoBehaviour {
 
         if (Input.GetKey("space"))
         {
-            if(walkedOver != null)
+            if (walkedOver != null)
             {
                 if (!walkedOver.CompareTag("Finish"))
                 {
@@ -74,17 +87,26 @@ public class PlayerController : MonoBehaviour {
                 }
             }
         }
+
+        Debug.Log(rb2d.velocity.x);
+    }
+
+    void ResetJump()
+    {
+        //Debug.Log(Time.time - jumpDelay);
+       // Debug.Log(GameObject.FindWithTag("otherColliders").GetComponent<BoxCollider2D>);
+        //if (Time.time - jumpDelay > 0.2) {
+            isGrounded = true;
+        //}
+        
     }
 
     void Jump()
     {
-        if (rb2d.velocity.y == 0)
+        if(rb2d.velocity.y == 0)
         {
-            {
-                rb2d.AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
-            }
+            rb2d.AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
         }
-
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -96,5 +118,15 @@ public class PlayerController : MonoBehaviour {
     {
         walkedOver = null;
     }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        
+        //  other.collider.IsTouching(Colli1);
+        if (!isGrounded)
+        {
+            ResetJump();
+        }
+    }
+    
 
 }
